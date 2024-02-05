bdb0947b5eb4ec8e0c51b598b92121783b97a53d
Waifuoniisan <96890280+Waifuoniisan@users.noreply.github.com>
2024-02-04 19:16:46 -0500

    Updated to student level design lectora

---
+++ Assets/Students/Anthony Lectora/scripts/Anthony_Invince.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anthony_Invince : GenericPower
+{
+    private float Cooldown = 3;
+    private float Invincible = 1.5f;
+
+    private bool isInvincible;
+
+    public override void Activate()
+    {
+        StartCoroutine(BecomeTemporarilyInvincible());
+
+    }
+    private IEnumerator BecomeTemporarilyInvincible()
+    {
+        Debug.Log("Player turned invincible!");
+        Player.GetComponent<PlayerController>().HP = 99999999f;
+          
+        yield return new WaitForSeconds(Invincible);
+
+        Player.GetComponent<PlayerController>().HP = 0;
+
+             
+        Debug.Log("Player is no longer invincible!");
+    }
+}
